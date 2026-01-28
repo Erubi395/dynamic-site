f438fc7e81aa9b7923e6e5f22ebdff7dd5c8dcac
badamkhatan erkhbilguun <badamkhatanerkhbilguun@badamkhatans-MacBook-Air.local>
2026-01-28 13:41:19 +0900

    update

--- public/news3.js
+++ public/news3.js
@@ -1,1 +1,27 @@
-console.log("Process page loaded");
+document.addEventListener("DOMContentLoaded", () => {
+  const newsItems = document.getElementById("newsItems");
+  
+  newsItems.innerHTML += newsItems.innerHTML;
+
+  let position = 0;
+  const speed = 0.5; 
+
+  function moveNews() {
+    position -= speed;
+
+    if (Math.abs(position) >= newsItems.scrollWidth / 2) {
+      position = 0;
+    }
+    newsItems.style.transform = `translateX(${position}px)`;
+    requestAnimationFrame(moveNews);
+  }
+      const links = document.querySelectorAll(".news-items a");
+    links.forEach(link => {
+        if (link.href.includes("news3.html")) {
+            link.style.color = "#ff5a5a";
+            link.style.fontWeight = "bold";
+        }
+    });
+
+  moveNews();
+});
---
+++ public/news6.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const newsItems = document.getElementById("newsItems");
+
+  newsItems.innerHTML += newsItems.innerHTML;
+
+  let x = 0;
+  const speed = 0.6;
+
+  function scrollNews() {
+    x -= speed;
+
+    if (Math.abs(x) >= newsItems.scrollWidth / 2) {
+      x = 0;
+    }
+    newsItems.style.transform = `translateX(${x}px)`;
+    requestAnimationFrame(scrollNews);
+  }
+  const links = document.querySelectorAll(".news-items a");
+    links.forEach(link => {
+        if (link.href.includes("news6.html")) {
+            link.style.color = "#ff5a5a";
+            link.style.fontWeight = "bold";
+        }
+    });
+  scrollNews();
+});
