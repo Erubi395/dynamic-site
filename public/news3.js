c9e497968a2663b23557c30881fa771e4acd9d01
badamkhatan erkhbilguun <badamkhatanerkhbilguun@badamkhatans-MacBook-Air.local>
2026-01-30 20:57:59 +0900

    update

--- public/news3.js
+++ public/news3.js
@@ -1,27 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const newsItems = document.getElementById("newsItems");
-  
-  newsItems.innerHTML += newsItems.innerHTML;
 
-  let position = 0;
-  const speed = 0.5; 
+  const links = newsItems.querySelectorAll("a");
+  const currentPath = "/news3"; 
 
-  function moveNews() {
-    position -= speed;
-
-    if (Math.abs(position) >= newsItems.scrollWidth / 2) {
-      position = 0;
-    }
-    newsItems.style.transform = `translateX(${position}px)`;
-    requestAnimationFrame(moveNews);
-  }
-      const links = document.querySelectorAll(".news-items a");
-    links.forEach(link => {
-        if (link.href.includes("/news3")) {
-            link.style.color = "#ff5a5a";
-            link.style.fontWeight = "bold";
-        }
-    });
-
-  moveNews();
+  links.forEach(link => {
+      if (link.getAttribute("href") === currentPath) {
+          link.style.color = "#ff5a5a";
+          link.style.fontWeight = "bold";
+          link.style.borderBottom = "2px solid #ff5a5a"; 
+      }
+  });
 });
